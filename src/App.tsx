e8505a881b69cb329b3508e14b5087fe6e38c4da
Mohan <mohan4u96@gmail.com>
2025-07-05 12:40:40 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -665,8 +665,7 @@ const MainApp: React.FC = () => {
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
       <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
+      <div className="flex-1 flex flex-col overflow-hidden">      
         <Header onEditProfile={() => setShowProfileEditor(true)} />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 dark:bg-gray-900 p-6">
           {renderContent()}
